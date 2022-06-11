bfac75e0b917b939fb555c661f3e8972575e3067
Fengshan Yu <95190262+Fengshan-yu@users.noreply.github.com>
2022-06-11 11:36:59 -0400

    Add files via upload

--- tic_tac_toe_players.py
+++ tic_tac_toe_players.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 display = """
@@ -20,7 +19,6 @@ display = """
 board = np.array([["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]])
 board_dict = {1: (0, 0), 2: (0, 1), 3: (0, 2), 4: (1, 0), 5: (1, 1), 6: (1, 2), 7: (2, 0), 8: (2, 1), 9: (2, 2)}
 
-
 def enter_move(board):
     # The function accepts the board's current status, asks the user about their move,
     # checks the input, and updates the board according to the user's decision.
@@ -29,15 +27,14 @@ def enter_move(board):
     board[board_dict[int(move)][0], board_dict[int(move)][1]] = f"{player_1}"
     del board_dict[int(move)]
 
-
 def victory_for(board, sign):
     # The function analyzes the board's status in order to check if
     # the player using 'sign' has won the game
     lines = [board[:, 0], board[:, 1], board[:, 2], board[0, :], board[1, :], board[2, :],
              [board[0, 0], board[1, 1], board[2, 2]], [board[2, 0], board[1, 1], board[0, 2]]]
-    for row in lines:
+    for line in lines:
         elements = []
-        for elem in row:
+        for elem in line:
             if elem == sign:
                 elements.append(elem)
         if len(elements) == 3:
@@ -54,8 +51,8 @@ def draw_move(board):
 print("Welcome to my game.")
 input("Please press enter to start")
 print(display)
-player_1 = input("Play 1, Please enter a character to use: ")
-player_2 = input("Play 2, Please enter a character to use: ")
+player_1 = input("Player 1, Please enter a character to use: ")
+player_2 = input("Player 2, Please enter a character to use: ")
 
 enter_move(board)
 display = display.replace(f"{move}", f"{player_1}")
