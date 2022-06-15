1d8a1a7852ab49002c8eb7d2db6e7aa6230c848d
Fengshan Yu <95190262+Fengshan-yu@users.noreply.github.com>
2022-06-15 13:07:39 -0400

    Add files via upload

--- tic_tac_toe_players.py
+++ tic_tac_toe_players.py
@@ -1,3 +1,4 @@
+# Set up the game with numpy.
 import numpy as np
 
 display = """
@@ -16,25 +17,26 @@ display = """
 +-------+-------+-------+
 """
 
-board = np.array([["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]])
-board_dict = {"1": (0, 0), "2": (0, 1), "3": (0, 2), "4": (1, 0), "5": (1, 1), "6": (1, 2), "7": (2, 0), "8": (2, 1), "9": (2, 2)}
+board = np.array([["1","2","3"],["4","5","6"],["7","8","9"]])
+board_dict = {"1":(0,0),"2":(0,1),"3":(0,2),"4":(1,0),"5":(1,1),
+              "6":(1,2),"7":(2,0),"8":(2,1),"9":(2,2)}
 
 def enter_move(board):
     # The function accepts the board's current status, asks the user about their move,
     # checks the input, and updates the board according to the user's decision.
     global move
     move = input("Player 1: please indicate your next move: ")
-    board[board_dict[move][0], board_dict[move][1]] = f"{player_1}"
+    board[board_dict[move][0],board_dict[move][1]] = f"{player_1}"
     del board_dict[move]
 
 def victory_for(board, sign):
     # The function analyzes the board's status in order to check if
     # the player using 'sign' has won the game
-    lines = [board[:, 0], board[:, 1], board[:, 2], board[0, :], board[1, :], board[2, :],
-             [board[0, 0], board[1, 1], board[2, 2]], [board[2, 0], board[1, 1], board[0, 2]]]
-    for line in lines:
+    lines = [board[:,0], board[:,1], board[:,2], board[0,:], board[1,:],
+             board[2,:], board[[0,1,2],[0,1,2]], board[[2,1,0],[0,1,2]]]
+    for row in lines:
         elements = []
-        for elem in line:
+        for elem in row:
             if elem == sign:
                 elements.append(elem)
         if len(elements) == 3:
@@ -51,8 +53,8 @@ def draw_move(board):
 print("Welcome to my game.")
 input("Please press enter to start")
 print(display)
-player_1 = input("Player 1, Please enter a character to use: ")
-player_2 = input("Player 2, Please enter a character to use: ")
+player_1 = input("Play 1, Please enter a character to use:")
+player_2 = input("Play 2, Please enter a character to use:")
 
 enter_move(board)
 display = display.replace(f"{move}", f"{player_1}")
@@ -63,14 +65,14 @@ while len(board_dict) > 0:
     display = display.replace(f"{step}", f"{player_2}")
     print(display)
     if victory_for(board, f"{player_2}"):
-        print("Play 2 won.")
+        print("Player 2 won.")
         break
 
     enter_move(board)
     display = display.replace(f"{move}", f"{player_1}")
     print(display)
     if victory_for(board, f"{player_1}"):
-        print("Play 1 won.")
+        print("Player 1 won.")
         break
 else:
     print("Game is over.")
